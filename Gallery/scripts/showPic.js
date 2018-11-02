9a4b3440a9b6c362a5b6411237955ca0c4c61ce2
zhangjiongjie <zhangjiongjie@sagereal.com>
2018-11-02 19:41:51 +0800

    optimization imageGallery 20181102
    Change-Id: I65fd5fcb0724467eb7f6a316bc6d5486a8b1835b

--- Gallery/scripts/showPic.js
+++ Gallery/scripts/showPic.js
@@ -1,8 +1,41 @@
-function showPic(whichPic){
-    var source = whichPic.getAttribute("href");
+function prepareGallery(){
+    if(!document.getElementsByTagName) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    if(!document.getElementById("placeholder")) return false;
+    var gallery = document.getElementById("imagegallery");
     var placeholder = document.getElementById("placeholder");
-    placeholder.setAttribute("src", source);
-    var text = whichPic.getAttribute("title");
-    var description = document.getElementById("description");
-    description.firstChild.nodeValue = text;
+    if(placeholder.nodeName != "IMG") return false;
+    var links = gallery.getElementsByTagName("a");
+    for(var i = 0; i < links.length; i++){
+       links [i].onclick = function(){
+            return !showPic(this, placeholder);
+        }
+    }
 }
+function showPic(whichPic, placeholder){
+    var source = whichPic.getAttribute("href");
+    placeholder.setAttribute("src", source);
+    
+    if(document.getElementById("description")){
+        var description = document.getElementById("description");
+        var text = whichPic.getAttribute("title") ? whichPic.getAttribute("title") : "";
+        if(description.firstChild.nodeType == 3){
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
+}
+
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof oldonload != 'function'){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
+addLoadEvent(prepareGallery);
