142d2a91abae708acb310a9388f91fb910b31f7c
zhangjiongjie <zhangjiongjie@sagereal.com>
2018-11-06 14:47:51 +0800

    Dynamically create tags 20181106
    Change-Id: Ib14b5a2d6ee7fe7a7c36fbf34d9adf9b5c8854d3

--- Gallery/scripts/showPic.js
+++ Gallery/scripts/showPic.js
@@ -38,4 +38,33 @@ function addLoadEvent(func){
         }
     }
 }
+//Dynamically create tags
+
+function preparePlaceholder(){
+    if(!document.createElement) return false;
+    if(!document.createTextNode) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/placeholder.gif");
+    placeholder.setAttribute("alt", "my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    var destext = document.createTextNode("Choose an image");
+    description.appendChild(destext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(placeholder, gallery);
+    insertAfter(description, placeholder);
+} 
+
+function insertAfter(newElement, targetElement){
+    var parent = targetElement.parentNode;
+    if(parent.lastChild == targetElement){
+        parent.appendChild(newElement);
+    }else{
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+addLoadEvent(preparePlaceholder);
+//Dynamically create tags 
 addLoadEvent(prepareGallery);
